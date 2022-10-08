dff82f1aa0806e7355433b5faaac47b9ba4a8f0f
Ahsan <omerjee.ahsan@gmail.com>
2022-10-09 01:02:52 +0500

    learn router

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
-import React, { useState,useEffect } from "react";
-import axios from "axios";
+import React from "react";
+// import axios from "axios";
+import { Routes,Route} from "react-router-dom";
 // import ComA from "./CompA";
 // const FirstName = createContext();
 // const LastName = createContext();
+import About from "./About";
+import Contact from "./Contact";
+import Error from "./Error";
 
 const App = () => {
 //   const [num, setnum] = useState(0);
@@ -44,42 +48,62 @@ const App = () => {
 //   );
 // };
 
-useEffect(()=>{
-  // alert('hi');
-  async function getdata(){
-    const res=await axios.get(`https://pokeapi.co/api/v2/pokemon/${num}`);
-    console.log(res.data.name);
-    console.log(res.data.moves);
-    setname1(res.data.name);
-    setmove(res.data.moves.length);
-    // setmove(res.data.moves[0].move.name);
+//**getting api using axios**
+// useEffect(()=>{
+//   // alert('hi');
+//   async function getdata(){
+//     const res=await axios.get(`https://pokeapi.co/api/v2/pokemon/${num}`);
+//     console.log(res.data.name);
+//     console.log(res.data.moves);
+//     setname1(res.data.name);
+//     setmove(res.data.moves.length);
+//     // setmove(res.data.moves[0].move.name);
 
-  }
-  getdata();
-})
-const[num,setnum]=useState();
-const[name1,setname1]=useState();
-const[move,setmove]=useState();
+//   }
+//   getdata();
+// })
+// const[num,setnum]=useState();
+// const[name1,setname1]=useState();
+// const[move,setmove]=useState();
 
 
 
+// return(
+// <>
+// <h1>You chose <span style={{color:"red"}}> {num} </span>value</h1>
+// <h1>My name is  <span style={{color:"red"}}>{name1}</span></h1>
+// <h1>I have  <span style={{color:"red"}}>{move}</span> moves</h1>
+
+// <select value={num} onChange={(event)=>{
+//       setnum(event.target.value)
+// }}>
+//   <option value="1">1</option>
+//   <option value="25">25</option>
+//   <option value="3">3</option>
+//   <option value="4">4</option>
+//   <option value="6">6</option>
+// </select>
+// </>
+// )
+
+
+//react router
 return(
-<>
-<h1>You chose <span style={{color:"red"}}> {num} </span>value</h1>
-<h1>My name is  <span style={{color:"red"}}>{name1}</span></h1>
-<h1>I have  <span style={{color:"red"}}>{move}</span> moves</h1>
+  <>
+{/* if only '/',it means root pr jana hai! */}
 
-<select value={num} onChange={(event)=>{
-      setnum(event.target.value)
-}}>
-  <option value="1">1</option>
-  <option value="25">25</option>
-  <option value="3">3</option>
-  <option value="4">4</option>
-  <option value="6">6</option>
-</select>
-</>
+<Routes>
+  <Route exact path="/" element={<About/>}  />
+  <Route path="/contact" element={<Contact/>} />
+  {/* if we want to show user a error when a user writes a path that doesnot exits so we give '*' in path in route tag */}
+  <Route path="*" element={<Error/>} />
+</Routes>
+
+  {/* <About/>
+  <Contact/> */}
+  </>
 )
+
 }
 
 
---
+++ src/Contact.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Contact=()=>{
+    return <h1>Hello,i am Contact page</h1>
+}
+
+export default Contact;
